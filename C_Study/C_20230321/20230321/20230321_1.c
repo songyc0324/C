9bf9e2dcb6a8fa9626204bb3c8201d2508a5aa17
songyc0324 <127912062+songyc0324@users.noreply.github.com>
2023-05-17 13:32:07 +0900

    Add files via upload

---
+++ C_Study/C_20230321/20230321/20230321_1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	// Title : 산술 연산자와 대입 연산자
+	int a, b;
+	int sum, sub, mul, inv;
+
+	a = 10;			// 대입 연산
+	b = 20;			// 대입 연산
+	sum = a + b;	// 더하기 연산 후 대입 연산
+	sub = a - b;	// 빼기 연산 후 대입 연산
+	mul = a * b;	// 곱하기 연산 후 대입 연산
+	inv = -a;		// 음수 연산 후 대입 연산
+
+	printf("a의 값 : %d, b의 값 : %d\n", a, b);
+	printf("덧셈 : %d\n", sum);
+	printf("뺄셈 : %d\n", sub);
+	printf("곱셈 : %d\n", mul);
+	printf("a의 음수연산 : %d\n", inv);
+	*/
+
+	/*
+	// Title : 나눗셈 연산자와 나머지 연산자
+	double apple;
+	int banana;
+	int orange;
+
+	apple = 5.0 / 2.0;		// 실수와 실수의 나눗셈 연산
+	banana = 5 / 2;			// 정수와 정수의 나눗셈 연산
+	orange = 5 % 2;			// 정수와 정수의 나머지 연산
+
+	printf("apple : %.1lf\n", apple);
+	printf("banana : %d\n", banana);
+	printf("orange : %d\n", orange);
+	*/
+
+
+	// Title : 증감 연산자
+	int a = 10, b = 10;
+
+	++a;		// 변수의 값을 1만큼 증가
+	--b;		// 변수의 값을 1만큼 감소
+
+	printf("a : %d\n", a);
+	printf("b : %d\n", b);
+
+
+
+	return 0;
+}
